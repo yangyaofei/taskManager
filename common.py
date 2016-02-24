8f3c315991cd20dd63cd2201afe8ad809fec996c
yangyaofei <yangyaofei@gmail.com>
2016-02-24 15:58:37 +0800

    daemonize argvs
    1 从Shadowsocks项目中移植daemon守护进程实现
    	已增加license声明
    2 增加参数用来在守护进程模式下运行
    
    3 TODO task运行的管理,进程的fock和管理

--- common.py
+++ common.py
@@ -29,4 +29,20 @@ class JsonEncoder(json.JSONEncoder):
             return obj.strftime('%Y-%m-%d')
         else:
             return json.JSONEncoder.default(self, obj)
+'''
+copy from shadowsocks common.py
+convert bytes and str in daemon.py
+'''
 
+def to_bytes(s):
+    if bytes != str:
+        if type(s) == str:
+            return s.encode('utf-8')
+    return s
+
+
+def to_str(s):
+    if bytes != str:
+        if type(s) == bytes:
+            return s.decode('utf-8')
+    return s
--- server.py
+++ server.py
@@ -2,33 +2,70 @@
 
 import socket
 import thread
+import os
+import sys
+sys.setdefaultencoding("utf-8")
+import getopt
+import logging
+reload(sys)
+
+import daemon
 import parse
 import request
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+
+logging.getLogger().setLevel(0)
 def dealClient(conn,addr):
 	szBuf = conn.recv(1024); 
-	print("get a request")
+	logging.info("get a request")
 	recv = request.routeRequest(szBuf)
 	conn.send(recv)	
-	print("respons the request over")
-if "__main__" == __name__:  
-	try:  
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);  
-		print("create socket succ!");  
-		sock.bind(('localhost', 8001));  
-		print("bind socket succ!");  
-          
-	        sock.listen(5);  
-		print("listen succ!");  
-  
-	except:  
-		print("init socket err!");  
-  
-	while True:  
-		print("listen for client...");  
-		conn, addr = sock.accept();  
-        	print("get client");  
-	        print(addr);  
-		thread.start_new_thread(dealClient,(conn,addr))
+	logging.info("respons the request over")
+def printHelp():
+	print("help")
+def printVersion():
+	print("version")
+
+shortopt = 'd:h'
+longopt = ["daemon=","help","version","pid-file=","log-file="]
+optlist, argvs = getopt.getopt(sys.argv[1:],shortopt,longopt)
+config = {}
+for key ,value in optlist:
+	if(key == "-d"):
+		config["daemon"] = value
+	elif(key == "-h"):
+		printHelp()
+		sys.exit(0)
+	elif(key == "--help"):
+		printHelp()
+		sys.exit(0)
+	elif(key == "--version"):
+		printVersion()
+		sys.exit(0)
+	elif(key == "--daemon"):
+		config["daemon"] = value
+	elif(key == "--pid-file"):
+		config["pid-file"] = value
+	elif(key == "--log-file"):
+		config["log-file"] = value
+config["pid-file"] = config.get("pid-file","/var/run/nlpServer.pid")
+config["log-file"] = config.get("log-file","/var/log/nlpServer.log")
+#print(config)	
+#print(optlist)	
+daemon.daemon_exec(config)
+try:  
+	logging.info("create socket succ!");  
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);  
+	sock.bind(('localhost', 8001));  
+	logging.info("bind socket succ!");  
+         
+        sock.listen(5);  
+	logging.info("listen succ!");  
+ 
+except:  
+	logging.error("init socket err!");  
+ 
+while True:  
+	logging.info("listen for client...");  
+	conn, addr = sock.accept();  
+       	logging.info("get client:"+str(addr));  
+	thread.start_new_thread(dealClient,(conn,addr))
